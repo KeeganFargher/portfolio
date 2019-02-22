fccfbae2069a650c70a67b8fb01227b1e4da4279
Keegan Fargher <fargherkeegan@gmail.com>
2019-02-22 19:57:22 +0200

    Cleaned up some code

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -2,44 +2,26 @@ let drops = [];
 const rainCount = 200;
 
 function setup() {
-    var canvas = createCanvas(windowWidth, windowHeight);
+	var canvas = createCanvas(windowWidth, windowHeight);
 
-    // Move the canvas so it’s inside our <div id="sketch-holder">.
-    canvas.parent('sketch-holder');
-    canvas.style('display', 'block');
-
-    // canvas.mousePressed(mousePressed);
-    // canvas.mouseReleased(mouseReleased);
+	// Move the canvas so it’s inside our <div id="sketch-holder">.
+	canvas.parent('sketch-holder');
+	canvas.style('display', 'block');
 }
 
 function draw() {
-    background(37, 41, 52);
+	background(37, 41, 52);
 
-    for (let drop of drops) {
-        drop.fall();
-        drop.show();
-    }
+	for (let drop of drops) {
+		drop.fall();
+		drop.show();
+	}
 
-    if (random() > 0.6 && drops.length < rainCount) {
-        drops.push(new Drop(windowWidth, windowHeight));
-    }
-
+	if (random() > 0.6 && drops.length < rainCount) {
+		drops.push(new Drop(windowWidth, windowHeight));
+	}
 }
 
 function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
-    //drops = [];
+	resizeCanvas(windowWidth, windowHeight);
 }
-
-// function mousePressed() {
-//     for (let drop of drops) {
-//         drop.speedDivider = 0.1;
-//     }
-
-// }
-
-// function mouseReleased() {
-//     for (let drop of drops) {
-//         drop.speedDivider = 1;
-//     }
-// }
