39ea947dccb084b2452715116208f0f452366dfe
Keegan Fargher <fargherkeegan@gmail.com>
2019-04-16 22:11:09 +0200

    contact me page

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,18 @@
 $('.loader-background').fadeOut();
 new WOW().init();
 
-scrollToWaypoint(".js--about-me", "#about-me");
-scrollToWaypoint(".js--timeline", "#timeline");
-scrollToWaypoint(".js--projects", "#projects");
-scrollToWaypoint(".js--contact-me", "#contact-me");
+scrollToWaypoint('.js--about-me', '#about-me');
+scrollToWaypoint('.js--timeline', '#timeline');
+scrollToWaypoint('.js--projects', '#projects');
+scrollToWaypoint('.js--contact-me', '#contact-me');
 
 function scrollToWaypoint(buttonId, sectionId) {
-    $(buttonId).click(function () {
-        $("html, body").animate({
-                scrollTop: $(sectionId).offset().top
-            },
-            500
-        );
-    });
+	$(buttonId).click(function() {
+		$('html, body').animate(
+			{
+				scrollTop: $(sectionId).offset().top
+			},
+			500
+		);
+	});
 }
