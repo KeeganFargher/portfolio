5244ef5dcfee3a4c4810e6ebd29425a734d7a38d
Keegan Fargher <fargherkeegan@gmail.com>
2019-04-21 12:00:04 +0200

    changed how the front page animation looks

--- scripts/drop.js
+++ scripts/drop.js
@@ -6,12 +6,14 @@ class Drop {
 		this.yPositionMin = 0;
 		this.yPositionMax = 0;
 
-		this.x = this.getRandomInt(0, width);
-		this.y = this.getRandomInt(this.yPositionMin, this.yPositionMax);
+		this.x = this.generateXCords();
+		this.y = this.generateYCords();
 
 		this.z = this.getRandomInt(2, 18);
 		this.length = map(this.z, 0, 20, 5, 15);
 		this.ySpeed = this.z / 3;
+
+		this.opacity = this.generateOpacity();
 
 		this.speedDivider = 1;
 	}
@@ -20,23 +22,25 @@ class Drop {
 		this.y += this.ySpeed * this.speedDivider;
 
 		if (this.y > height) {
-			this.y = this.getRandomInt(this.yPositionMin, this.yPositionMax);
-			this.x = this.getRandomInt(0, width);
+			this.y = this.generateYCords();
+			this.x = this.generateXCords();
 			this.ySpeed = this.z / 3;
+			this.opacity = this.generateOpacity();
 		}
 
 		if (this.x > width || this.x < 0) {
-			this.x = this.getRandomInt(0, width);
+			this.x = this.generateXCords();
 		}
 	}
 
 	show() {
-		let thickness = map(this.z, 0, 20, 0.5, 3);
-		let opacity = map(this.z, 0, 20, 50, 200);
+		let thickness = map(this.z, 0, 20, 1, 3);
 
 		strokeWeight(thickness);
-		stroke(95, 102, 119, opacity);
+		stroke(141, 146, 160, this.opacity);
 		line(this.x, this.y, this.x, this.y + this.length);
+
+		this.opacity /= 1.02;
 	}
 
 	getRandomInt(min, max) {
@@ -44,4 +48,8 @@ class Drop {
 		max = Math.floor(max);
 		return Math.floor(Math.random() * (max - min + 1)) + min;
 	}
+
+	generateYCords = () => this.getRandomInt(this.yPositionMin, this.yPositionMax);
+	generateXCords = () => this.getRandomInt(0, width);
+	generateOpacity = () => map(this.z, 0, 20, 100, 200);
 }
